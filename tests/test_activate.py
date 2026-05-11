6b2fdb84302c7eb464fae2762309f2f6159b26b2
Jannis Leidel <jannis@leidel.info>
2026-05-11 21:39:48 +0200

    Apply suggestions from code review
    Co-authored-by: Marco Esters <mesters@anaconda.com>

--- tests/test_activate.py
+++ tests/test_activate.py
@@ -6,9 +6,6 @@ from conda.common.compat import on_win
 from conda_spawn.shell import PowershellShell
 
 
-@pytest.mark.skipif(
-    not on_win, reason="PowerShell conda function only needed on Windows"
-)
 def test_powershell_script_defines_conda_function(simple_env):
     """PowerShell script must define a conda function routing to conda.exe (#32).
 
@@ -22,12 +19,4 @@ def test_powershell_script_defines_conda_function(simple_env):
     conda_exe = context.conda_exe_vars_dict["CONDA_EXE"]
     shell = PowershellShell(simple_env)
     script = shell.script()
-    assert f'function conda {{ & "{conda_exe}" @args }}' in script
-
-
-@pytest.mark.skipif(on_win, reason="conda function should not be injected on Unix")
-def test_powershell_script_no_conda_function_on_unix(simple_env):
-    """On non-Windows platforms the conda function is not injected."""
-    shell = PowershellShell(simple_env)
-    script = shell.script()
-    assert "function conda" not in script
+    assert (f'function conda {{ & "{conda_exe}" @args }}' in script) == on_win
